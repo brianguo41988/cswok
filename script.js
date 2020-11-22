61da1557a893cb7d3e75b05bf6369789811cf226
brianguo41988 <54339742+brianguo41988@users.noreply.github.com>
2020-11-22 14:14:54 -0500

    added in require

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 
-// const menu_data = require('data-store')({ path: process.cwd() + '/data/menu.json' });
+const menu_data = require('data-store')({ path: process.cwd() + '/data/menu.json' });
 
-// let hello = new Menu(Menu.next_id, "hello", 3.4);
-// menu_data.set(hello.id.toString(), hello);
+let hello = new Menu(Menu.next_id, "hello", 3.4);
+menu_data.set(hello.id.toString(), hello);
 console.log("added hello");
