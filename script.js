c7749c1c96fd0bb60137e50ccab49f7eb4d8a925
brianguo41988 <54339742+brianguo41988@users.noreply.github.com>
2020-11-22 21:08:08 -0500

    git push

--- script.js
+++ script.js
@@ -2,12 +2,50 @@ async function replySubmit(number){
     console.log(number);
 }
 
-const result = await axios({
-    method: 'post',
-    url: 'https://cswok.herokuapp.com/menu',
-    withCredentials: true,
-    data: {
-      "name": "number",
-      "price": 8
-    },
-  });
+// async function fn1() {	
+//     const result = await axios({
+//         method: 'get',
+//         url: 'https://cswok.herokuapp.com/menu',
+//         withCredentials: true,	
+//       });	
+//       console.log(result);
+//     }
+// fn1();
+async function fn2() {	
+const result = await axios({	
+    method: 'post',	
+    url: 'https://cswok.herokuapp.com/menu',	
+    withCredentials: true,	
+    data: {	
+      "name": "Chuicke Brocili",	
+      "price": 4.50	
+    },	
+  });	
+}	
+fn2();	
+console.log("it worked?");	
+async function fn3() {	
+    const result = await axios({	
+        method: 'post',	
+        url: 'https://cswok.herokuapp.com/menu',	
+        withCredentials: true,	
+        data: {	
+          "name": "Chuicke Brocili",	
+          "price": 4.50	
+        },	
+      });	
+    }	
+    fn3();
+    async function fn4() {	
+        const result = await axios({	
+            method: 'post',	
+            url: 'https://cswok.herokuapp.com/menu',	
+            withCredentials: true,	
+            data: {	
+              "name": "Chuicke Brocili",	
+              "price": 4.50	
+            },	
+          });	
+        }	
+        fn4();	
+console.log("it worked?");
