6a8690a1018cf6ddcdbe245f9080c58b9cccf81d
RiisWidner <54446775+RiisWidner@users.noreply.github.com>
2020-11-23 13:30:11 -0500

    search

---
+++ autocomplete.js
@@ -0,0 +1,11 @@
+let Items = [
+    ];
+    
+    $('#form-autocomplete-4').mdbAutocomplete({
+    data: items,
+    dataColor: 'green',
+    inputFocus: '2px solid green',
+    inputBlur: '1px solid #ced4da',
+    inputFocusShadow: '0 1px 0 0 #4285f4',
+    inputBlurShadow: ''
+    });
