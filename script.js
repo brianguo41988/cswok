6f16b6cf65dd04edf87fce5c7e8f3da0017c4d97
brianguo41988 <54339742+brianguo41988@users.noreply.github.com>
2020-11-22 21:03:06 -0500

    git push

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
 async function replySubmit(number){
     console.log(number);
 }
+
+const result = await axios({
+    method: 'post',
+    url: 'https://cswok.herokuapp.com/menu',
+    withCredentials: true,
+    data: {
+      "name": "number",
+      "price": 8
+    },
+  });
