14141a3ddeaeca3ff139980065e0766373d37274
RiisWidner <54446775+RiisWidner@users.noreply.github.com>
2020-11-23 11:29:53 -0500

    loginscript

--- loginscript.js
+++ loginscript.js
@@ -1,1 +1,31 @@
 console.log("entered login script");
+async function loginFn(number, name){
+    const result = await axios({	
+        method: 'post',	
+        url: 'https://cswok.herokuapp.com/menu',	
+        withCredentials: true,	
+        data: {	
+          "name": `${name}`,	
+          "price": `${number}`,
+          "menu": false
+        },	
+      });	
+    }
+    async function fn1() {
+        console.log("entered fn1");
+        const result = await axios({
+                    method: 'get',
+                    url: 'https://cswok.herokuapp.com/menu',
+                    withCredentials: true,	
+                  });	
+        for (let i = 0; i < result.data.length; i++){
+            const result2 = await axios({
+                method: 'get',
+                url: `https://cswok.herokuapp.com/menu/${i}`,
+                withCredentials: true,	
+              });	
+              for (let i = 0; i < result2.data.length; i++){
+              console.log(result2.data.name);
+              }
+        }
+    }
